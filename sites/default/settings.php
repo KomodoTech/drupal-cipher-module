927a091197bfefb106ffd1535964d4a6e25afb33
alexandre <alexandre.leibler@gmail.com>
2016-12-05 10:22:32 -0500

    create basic form validation

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'cipher',
+      'username' => 'cipher',
+      'password' => 'cipher',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'Uf2Hx6mJNCuTuu4SWaWoe45TQ3vRL-58biH1mwHYoJY';
 
 /**
  * Base URL (optional).
